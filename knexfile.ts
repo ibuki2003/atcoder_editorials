813b6e7c1ce28f59d12cdfc161b3f0ea0920b84b
ibuki2003 <main@fuwa.dev>
2020-07-01 09:32:43 +0900

    chore: setup knex

---
+++ knexfile.ts
@@ -0,0 +1,29 @@
+// Update with your config settings.
+
+import Knex from "knex";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const conf: Knex.Config = {
+  client: process.env.DB_CLIENT,
+  connection: {
+    filename: process.env.DB_FILENAME,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    db: process.env.DB_NAME,
+    charset: "utf8",
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    directory: "./db/migrations",
+    tableName: "knex_migrations",
+    extension: "ts",
+  },
+};
+
+module.exports = conf;
